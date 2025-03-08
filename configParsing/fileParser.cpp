9960faba99caa747199a8222997a2314a399dda1
Youssef ElMountahi <yusuf.one@outlook.com>
2025-03-08 16:06:25 +0000

    fixing extention check and checking file exectence

--- configParsing/fileParser.cpp
+++ configParsing/fileParser.cpp
@@ -6,7 +6,7 @@
 /*   By: yel-moun <yel-moun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 14:56:59 by yel-moun          #+#    #+#             */
-/*   Updated: 2025/03/08 15:51:27 by yel-moun         ###   ########.fr       */
+/*   Updated: 2025/03/08 16:04:55 by yel-moun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,10 @@ bool FileParser::checkFileExtension()
 	size_t dotPos = _filename.find_last_of(".");
 	if (dotPos == std::string::npos)
 		return false;
-	if (_filename.find(FILE_EXTENSION, dotPos) == std::string::npos)
+	size_t extPos = _filename.find(FILE_EXTENSION);
+	if (extPos == std::string::npos)
+		return false;
+	if (extPos + 5 != _filename.length())
 		return false;
 	return true;
 }
@@ -33,6 +36,16 @@ void FileParser::start()
 {
 	if (!this->checkFileExtension())
 		throw WrongExtentionException();
+	if (!this->checkFileExistence())
+		throw FileDoesNotExistException();
+}
+
+bool FileParser::checkFileExistence()
+{
+	this->_configFile.open(_filename);
+	if (!this->_configFile.good())
+		return false;
+	return true;
 }
 
 void FileParser::parseFile()
@@ -53,4 +66,9 @@ const char *FileParser::WrongFileFormatException::what() const throw()
 const char *FileParser::WrongFileException::what() const throw()
 {
 	return "Please provide a valid file as argument";
+}
+
+const char *FileParser::FileDoesNotExistException::what() const throw()
+{
+	return "File does not exist";
 }
--- configParsing/fileParser.hpp
+++ configParsing/fileParser.hpp
@@ -6,7 +6,7 @@
 /*   By: yel-moun <yel-moun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 15:29:26 by yel-moun          #+#    #+#             */
-/*   Updated: 2025/03/08 15:46:01 by yel-moun         ###   ########.fr       */
+/*   Updated: 2025/03/08 15:56:17 by yel-moun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 #define FILEPARSER_HPP
 
 #include <iostream>
+#include <fstream>
 #define FILE_EXTENSION ".conf"
 class FileParser
 {
 private:
 	std::string _filename;
+	std::ifstream _configFile;
+	bool checkFileExtension();
+	bool checkFileExistence();
 
 public:
 	FileParser(int argc, char **argv);
 	void start();
 	void parseFile();
-	bool checkFileExtension();
 	class WrongExtentionException : public std::exception
 	{
 		virtual const char *what() const throw();
@@ -34,6 +37,10 @@ public:
 		virtual const char *what() const throw();
 	};
 	class WrongFileException : public std::exception
+	{
+		virtual const char *what() const throw();
+	};
+	class FileDoesNotExistException : public std::exception
 	{
 		virtual const char *what() const throw();
 	};
